3e114c089f6900508d0a9fc037a559ab897bd8aa
joaopps1974 <joao.pereira.ismart@gmail.com>
2026-07-20 00:22:17 -0300

    Sprint 12.1: Adicionando valor para cada slot de inventário

--- inventory/slot.gd
+++ inventory/slot.gd
@@ -1,6 +1,15 @@
 extends Panel
 
+@export var quantidade_inicial: int = 10
+
+var quantidade: int
+
 @onready var item_visual: TextureRect = $CenterContainer/Panel/itemdisplay
+@onready var quantidade_label = $CenterContainer/Panel/Label
+
+func _ready():
+	quantidade = quantidade_inicial
+	quantidade_label.text = str(quantidade)
 
 func update(item: InvItem):
 	if !item:
@@ -8,3 +17,14 @@ func update(item: InvItem):
 	else:
 		item_visual.visible = true
 		item_visual.texture = item.texture
+
+func usar_item():
+	if quantidade <= 0:
+		return
+
+	quantidade -= 1
+	quantidade_label.text = str(quantidade)
+
+	if quantidade <= 0:
+		# opcional: o que fazer quando o slot esvazia
+		pass
